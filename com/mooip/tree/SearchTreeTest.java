8fdbded92464e71e25a458ba1c4f3edbe1cf3809
Masterofoneinchpunch <126126947+Masterofoneinchpunch@users.noreply.github.com>
2023-03-01 12:29:44 -0800

    Added BFS support.

--- com/mooip/tree/SearchTreeTest.java
+++ com/mooip/tree/SearchTreeTest.java
@@ -1,12 +1,15 @@
 package com.mooip.code.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Search Test Tree.  This will show various algorithms for searching a tree.
+ * DFS means depth-first search. BFS means breadth-first search.
  * 
  * @author masterofoneinchpunch
  */
 public final class SearchTreeTest {
-
     private static int count = 0;
     
     private static void dFSPreOrder(BinaryTree node) {
@@ -39,6 +42,28 @@ public final class SearchTreeTest {
         visit(node);
     }
 
+    private static void bfs(BinaryTree root) {
+        Queue<BinaryTree> queue = new LinkedList<BinaryTree>();
+        queue.offer(root);
+        int countBfs = 0;
+        while (queue.isEmpty() == false) {
+            final int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                BinaryTree temp = queue.poll();
+                System.out.print(temp.getValue() + " "); //prints in level order
+                countBfs++;
+                if (temp.getLeft() != null) {
+                    queue.offer(temp.getLeft());
+                }
+                
+                if (temp.getRight() != null) {
+                    queue.offer(temp.getRight());
+                }
+            }
+        }
+        System.out.println("countBfs: " + countBfs);
+    }
+      
     private static void visit(BinaryTree node){
         System.out.println("visit: " + node.getValue());
         count++;
@@ -62,11 +87,14 @@ public final class SearchTreeTest {
         dFSPostOrder(root);
         System.out.println("DFS PostOrder Count: " + count);    
         count = 0;
+        
         //in order is the only one where all the values shown will be to the left of the
         //root node and all the values to the right will be shown to the right of the root node.
         System.out.println("IN ORDER: "); //15,1,3,4 
         dFSInOrder(root);
         System.out.println("DFS InOrder Count: " + count);    
+        
+        bfs(root);            
     }  
 }
 
