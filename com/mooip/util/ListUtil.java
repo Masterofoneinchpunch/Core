52e066c0063a0b9d66af429eacf690bad64e32f3
Masterofoneinchpunch <126126947+Masterofoneinchpunch@users.noreply.github.com>
2023-03-01 17:04:59 -0800

    Modified JavaDocs

--- com/mooip/util/ListUtil.java
+++ com/mooip/util/ListUtil.java
@@ -3,6 +3,11 @@ package com.mooip.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An utility class for lists.
+ * 
+ * @author smckenna
+ */
 public final class ListUtil {
     private ListUtil() {
     }
@@ -23,6 +28,12 @@ public final class ListUtil {
         return arr;
     }
     
+    /**
+     * Changes an integer list into an int array.
+     * 
+     * @param arr The integer array.
+     * @return newList The Integer list.
+     */
     public static List<Integer> toList(int[] arr) {
         List<Integer> newList = new ArrayList<Integer>();
         for (Integer num : arr) {
@@ -32,6 +43,13 @@ public final class ListUtil {
         return newList;
     }
     
+    /**
+     * Returns all the items from the first list that is not in the second list.
+     * 
+     * @param list1 The first list.
+     * @param list2 The second list.
+     * @return minus A list of all the first list items not in the second list.
+     */
     public static <T> List<T> minus(List<T> list1, List<T> list2) {
         List<T> minus = new ArrayList<T>();
         for (T item : list1) {
@@ -42,6 +60,12 @@ public final class ListUtil {
         return minus;
     }
 
+    /**
+     * Takes a list in order and concatenates a number.
+     * 
+     * @param list The list full of Integers.
+     * @return number The result of the concatenation of Integers.
+     */
     public static Long listToNumber(List<Integer> list) {
         StringBuilder sb = new StringBuilder(list.size());
         for (Integer num : list) {
@@ -51,4 +75,3 @@ public final class ListUtil {
         return Long.valueOf(sb.toString());
     }  
 }
-
